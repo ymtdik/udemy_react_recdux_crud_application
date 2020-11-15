38fea80ae1495bfe3124ef15d1aae59283a72f92
ymtdik <daiki.yama.1987@gmail.com>
2020-11-15 12:14:01 +0900

    learn jsx syntax

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onChange={() => {
+            console.log("I am clicked!");
+          }}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
