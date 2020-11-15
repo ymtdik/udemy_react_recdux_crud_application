d864ea21957fdf255027d9ebb80c96dad537a95e
ymtdik <daiki.yama.1987@gmail.com>
2020-11-15 14:21:29 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { createState } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
-import App from "./App";
+
+import "./index.css";
+import reducer from "./reducers";
+
+import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
