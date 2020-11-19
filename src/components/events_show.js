b901e381b9f3e8eccd9899883b9699418fad339b
ymtdik <daiki.yama.1987@gmail.com>
2020-11-19 21:43:14 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,13 +3,20 @@ import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
-import { postEvent } from "../actions";
+import { getEvent, deleteEvent, putEvent } from "../actions";
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    if (id) this.props.getEvent(id);
+  }
+
   renderField(field) {
     const {
       input,
@@ -26,8 +33,14 @@ class EventsNew extends Component {
     );
   }
 
+  async onDeleteClick() {
+    const { id } = this.props.match.params;
+    await this.props.deleteEvent(id);
+    this.props.history.push("/");
+  }
+
   async onSubmit(values) {
-    await this.props.postEvent(values);
+    await this.props.putEvent(values);
     this.props.history.push("/");
   }
 
@@ -60,6 +73,9 @@ class EventsNew extends Component {
             disabled={pristine || submitting || invalid}
           />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>
+            Delete
+          </Link>
         </div>
       </form>
     );
@@ -74,9 +90,17 @@ const validate = (values) => {
 
   return errors;
 };
-const mapDispatchToProps = { postEvent };
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id];
+  return { initialValues: event, event };
+};
+const mapDispatchToProps = { deleteEvent, getEvent, putEvent };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
-)(reduxForm({ validate, form: "eventNewForm" })(EventsNew));
+)(
+  reduxForm({ validate, form: "eventShowForm", enableReinitialize: true })(
+    EventsShow
+  )
+);
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,26 @@
 import _ from "lodash";
-import { READ_EVENTS } from "../actions";
+import {
+  CREATE_EVENT,
+  READ_EVENTS,
+  READ_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+} from "../actions";
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data;
+      console.log(action.response.data);
+      // {id: 10, title: "Let's have an event 10!", body: "This is the body for event 10."}
+      return { ...events, [data.id]: data };
     case READ_EVENTS:
       return _.mapKeys(action.response.data, "id");
+    case DELETE_EVENT:
+      delete events[action.id];
+      return { ...events };
     default:
       return events;
   }
